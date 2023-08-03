65fea14b691e8026ae8b1d4ab0621121134e623b
Paperkeem <ahharlawhddl@gmail.com>
2023-08-03 17:05:30 +0900

    decodeDangleToken remove

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,15 @@
-import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import appendHeaderTitle from './utils/middleware/hooks/appendHeaderProps';
+import protectedURLs from './utils/middleware/hooks/protectedURLs';
 
 export async function middleware(req: NextRequest) {
   const requestHeaders = new Headers(req.headers);
 
-  appendHeaderTitle({ req, requestHeaders });
-
+  const adminURLsResult = protectedURLs({ req, requestHeaders });
+  if (adminURLsResult.redirect) {
+    return adminURLsResult.response;
+  }
   appendHeaderTitle({ req, requestHeaders });
 
   return NextResponse.next({
