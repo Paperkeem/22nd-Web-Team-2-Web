757b1345e6b1d874872ba64c0e06b5900be6c029
Paperkeem <ahharlawhddl@gmail.com>
2023-08-03 16:58:33 +0900

    미들웨어 삭제

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,9 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import appendHeaderTitle from './utils/middleware/hooks/appendHeaderProps';
-import protectedURLs from './utils/middleware/hooks/protectedURLs';
 
 export async function middleware(req: NextRequest) {
   const requestHeaders = new Headers(req.headers);
-
-  const adminURLsResult = protectedURLs({ req, requestHeaders });
-  if (adminURLsResult.redirect) {
-    return adminURLsResult.response;
-  }
 
   appendHeaderTitle({ req, requestHeaders });
 
